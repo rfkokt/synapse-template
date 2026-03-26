f5d1c4c68ab2cfa2fc13dffccaa44adeea7ade96
Rifki Okta <rifkiokta105@gmail.com>
2026-03-26 16:19:27 +0700

    chore: remove reporting-mfe module configuration and its associated route

--- apps/shell/src/router.tsx
+++ apps/shell/src/router.tsx
@@ -13,7 +13,6 @@ const RemoteRegister = lazy(() => import('authMfe/RegisterPage'));
 const RemoteProfile = lazy(() => import('authMfe/ProfilePage'));
 
 const RemoteDocs = lazy(() => import('docsmfe/App'));
-const RemoteReportingmfe = lazy(() => import('reportingmfe/App'));
 
 function getSafeRedirectFromQuery(): string | null {
   if (typeof window === 'undefined') {
@@ -123,14 +122,6 @@ export function AppRouter() {
           element={
             <RemoteLoader>
               <RemoteDocs />
-            </RemoteLoader>
-          }
-        />
-        <Route
-          path="reporting-mfe/*"
-          element={
-            <RemoteLoader>
-              <RemoteReportingmfe />
             </RemoteLoader>
           }
         />
